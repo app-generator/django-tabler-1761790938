b3dfdf552ba6880b53a8487312f590fa12dd6ade
App Generator <51070104+app-generator@users.noreply.github.com>
2025-10-30 04:22:27 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,48 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+    name = models.TextField(max_length=255, null=True, blank=True)
+    profile_picture = models.TextField(max_length=255, null=True, blank=True)
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Paramater(models.Model):
+
+    #__Paramater_FIELDS__
+    title = models.TextField(max_length=255, null=True, blank=True)
+    key = models.TextField(max_length=255, null=True, blank=True)
+    value = models.TextField(max_length=255, null=True, blank=True)
+    kind = models.TextField(max_length=255, null=True, blank=True)
+    expires = models.DateTimeField(blank=True, null=True, default=timezone.now)
+
+    #__Paramater_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Paramater")
+        verbose_name_plural = _("Paramater")
+
+
+
+#__MODELS__END
